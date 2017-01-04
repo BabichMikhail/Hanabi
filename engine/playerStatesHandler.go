6953777230d273671f97c6e2ddd41e62766d3e9d
Babich Mikhail <Babich-MV@yandex.ru>
2017-01-04 18:27:30 +1000

    Engine: Remove unnecessary fields in PlayerState

--- engine/playerStatesHandler.go
+++ engine/playerStatesHandler.go
@@ -1,28 +1,14 @@
 package engine
 
-type ColorInfo struct {
-	Color CardColor `json:"color"`
-	Count int       `json:"count"`
-}
-
-type ValueInfo struct {
-	Value CardColor `json:"value"`
-	Count int       `json:"count"`
-}
-
 type PlayerState struct {
-	PlayerId       int         `json:"player_id"`
-	PlayerPosition int         `json:"pos"`
-	PlayersCards   [][]Card    `json:"players_cards"`
-	ColorInfo      []ColorInfo `json:"color_info"`
-	ValueInfo      []ValueInfo `json:"value_info"`
+	PlayerId       int      `json:"player_id"`
+	PlayerPosition int      `json:"pos"`
+	PlayersCards   [][]Card `json:"players_cards"`
 }
 
 func NewPlayerState(cards [][]Card, playerPosition int, playerId int) PlayerState {
 	this := new(PlayerState)
 	this.PlayerPosition = playerPosition
-	this.ColorInfo = []ColorInfo{}
-	this.ValueInfo = []ValueInfo{}
 	this.PlayerId = playerId
 
 	copyCards := [][]Card{}
