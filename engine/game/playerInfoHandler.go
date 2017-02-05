8f82987b94b1d9fe3742b8b5de434c053491f012
Babich Mikhail <Babich-MV@yandex.ru>
2017-02-05 19:37:58 +1000

    Add hidden information other players

--- engine/game/playerInfoHandler.go
+++ engine/game/playerInfoHandler.go
@@ -1,18 +1,21 @@
 package game
 
+import "fmt"
+
 type PlayerGameInfo struct {
-	MyTurn       bool               `json:"my_turn"`
-	PlayerCount  int                `json:"player_count"`
-	Position     int                `json:"pos"`
-	Step         int                `json:"step"`
-	Round        int                `json:"round"`
-	PlayerId     int                `json:"player_id"`
-	DeckSize     int                `json:"deck_size"`
-	UsedCards    []Card             `json:"used_cards"`
-	TableCards   map[CardColor]Card `json:"table_cards"`
-	PlayersCards [][]Card           `json:"players_cards"`
-	BlueTokens   int                `json:"blue_tokens"`
-	RedTokens    int                `json:"red_tokens"`
+	MyTurn           bool               `json:"my_turn"`
+	PlayerCount      int                `json:"player_count"`
+	Position         int                `json:"pos"`
+	Step             int                `json:"step"`
+	Round            int                `json:"round"`
+	PlayerId         int                `json:"player_id"`
+	DeckSize         int                `json:"deck_size"`
+	UsedCards        []Card             `json:"used_cards"`
+	TableCards       map[CardColor]Card `json:"table_cards"`
+	PlayersCards     [][]Card           `json:"players_cards"`
+	PlayersCardsInfo [][]Card           `json:"players_cards_info"`
+	BlueTokens       int                `json:"blue_tokens"`
+	RedTokens        int                `json:"red_tokens"`
 }
 
 func (this *Game) GetPlayerGameInfo(playerId int) PlayerGameInfo {
@@ -24,34 +27,49 @@ func (this *Game) GetPlayerGameInfo(playerId int) PlayerGameInfo {
 		}
 	}
 
-	for i := 0; i < len(playerState.PlayersCards); i++ {
-		if playerState.PlayerPosition == i {
-			cards := &playerState.PlayersCards[i]
-			for j := 0; j < len(*cards); j++ {
-				card := &(*cards)[j]
-				if !card.KnownColor {
-					card.Color = NoneColor
-				}
-				if !card.KnownValue {
-					card.Value = NoneValue
-				}
+	playersCardsInfo := [][]Card{}
+	fmt.Println(state.PlayerCount)
+	for i := 0; i < state.PlayerCount; i++ {
+		cards := []Card{}
+		playerCards := state.PlayerStates[i].PlayersCards[i]
+		for j := 0; j < len(playerCards); j++ {
+			card := playerCards[j].Copy()
+			if !card.KnownColor {
+				card.Color = NoneColor
 			}
+			if !card.KnownValue {
+				card.Value = NoneValue
+			}
+			cards = append(cards, card)
+		}
+		playersCardsInfo = append(playersCardsInfo, cards)
+	}
+
+	cards := playerState.PlayersCards[playerState.PlayerPosition]
+	for j := 0; j < len(cards); j++ {
+		card := &cards[j]
+		if !card.KnownColor {
+			card.Color = NoneColor
+		}
+		if !card.KnownValue {
+			card.Value = NoneValue
 		}
 	}
 
 	return PlayerGameInfo{
-		MyTurn:       state.CurrentPosition == playerState.PlayerPosition,
-		PlayerCount:  this.PlayerCount,
-		Position:     playerState.PlayerPosition,
-		Step:         state.Step,
-		Round:        state.Round,
-		PlayerId:     playerState.PlayerId,
-		DeckSize:     len(state.Deck),
-		UsedCards:    state.UsedCards,
-		TableCards:   state.TableCards,
-		PlayersCards: playerState.PlayersCards,
-		BlueTokens:   state.BlueTokens,
-		RedTokens:    MaxRedTokens - state.RedTokens,
+		MyTurn:           state.CurrentPosition == playerState.PlayerPosition,
+		PlayerCount:      this.PlayerCount,
+		Position:         playerState.PlayerPosition,
+		Step:             state.Step,
+		Round:            state.Round,
+		PlayerId:         playerState.PlayerId,
+		DeckSize:         len(state.Deck),
+		UsedCards:        state.UsedCards,
+		TableCards:       state.TableCards,
+		PlayersCards:     playerState.PlayersCards,
+		PlayersCardsInfo: playersCardsInfo,
+		BlueTokens:       state.BlueTokens,
+		RedTokens:        MaxRedTokens - state.RedTokens,
 	}
 }
 
