2dc84c514373086c082a2dee9492fa1c2d15f8a0
Babich Mikhail <Babich-MV@yandex.ru>
2017-02-22 00:58:34 +1000

    Lobby: Remove unnecessary params

--- controllers/lobby.go
+++ controllers/lobby.go
@@ -17,13 +17,7 @@ func (this *LobbyController) GameList() {
 	var user wetalk.User
 	auth.GetUserFromSession(&user, this.Ctx.Input.CruSession)
 	this.Data["user"] = user
-	games := models.GetGameList([]int{lobby.GameActive, lobby.GameWait}, user.Id)
-
-	copyGames := lobby.CopyGameItems(games)
-	gameCount := len(games)
-	this.Data["gamesLeft"] = lobby.RevertGameItems(copyGames[gameCount/2:])
-	this.Data["gamesRight"] = lobby.RevertGameItems(copyGames[:gameCount/2])
-	this.Data["games"] = games
+	this.Data["games"] = models.GetGameList([]int{lobby.GameActive, lobby.GameWait}, user.Id)
 
 	this.LayoutSections = make(map[string]string)
 	this.LayoutSections["Header"] = "components/navbar.html"
