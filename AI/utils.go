372eb5139e90195ff404b78c1286c22348f587aa
Babich Mikhail <Babich-MV@yandex.ru>
2017-03-29 19:40:30 +1000

    AI: Refactoring utils

--- AI/utils.go
+++ AI/utils.go
@@ -19,32 +19,19 @@ func Min(a, b int) int {
 }
 
 func DefaultUsernamePrefix(AIType int) string {
-	switch AIType {
-	case Type_AIRandom:
-		return AI_NamePrefix + Name_AIRandom
-	case Type_AISmartyRandom:
-		return AI_NamePrefix + Name_AISmartyRandom
-	case Type_AIDiscardUsefulCard:
-		return AI_NamePrefix + Name_AIDiscardUsefulCard
-	case Type_AIUsefulInformation:
-		return AI_NamePrefix + Name_AIUsefulInformation
-	case Type_AIUsefulInformationV2:
-		return AI_NamePrefix + Name_AIUsefulInformationV2
+	for aiType, aiName := range AINames {
+		if aiType == AIType {
+			return AI_NamePrefix + aiName
+		}
 	}
-	panic("Bad AI_Type")
+	panic("Bad AIType")
 }
 
 func GetAITypeByUserNickName(nickname string) int {
-	if ok, _ := regexp.MatchString(AI_NamePrefix+Name_AIRandom+"_\\d", nickname); ok {
-		return Type_AIRandom
-	} else if ok, _ := regexp.MatchString(AI_NamePrefix+Name_AISmartyRandom+"_\\d", nickname); ok {
-		return Type_AISmartyRandom
-	} else if ok, _ := regexp.MatchString(AI_NamePrefix+Name_AIDiscardUsefulCard+"_\\d", nickname); ok {
-		return Type_AIDiscardUsefulCard
-	} else if ok, _ := regexp.MatchString(AI_NamePrefix+Name_AIUsefulInformation+"_\\d", nickname); ok {
-		return Type_AIUsefulInformation
-	} else if ok, _ := regexp.MatchString(AI_NamePrefix+Name_AIUsefulInformationV2+"_\\d", nickname); ok {
-		return Type_AIUsefulInformationV2
+	for aiType, aiName := range AINames {
+		if ok, _ := regexp.MatchString(AI_NamePrefix+aiName+"_\\d", nickname); ok {
+			return aiType
+		}
 	}
 	panic("Bad nickname")
 }
