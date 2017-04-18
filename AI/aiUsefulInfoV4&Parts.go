efe5235b86fbd64f0be40aa5773bf15100f69276
Babich Mikhail <Babich-MV@yandex.ru>
2017-04-18 23:28:34 +1000

    AI: Imrpove AIUsefulInfoV4&Parts

--- AI/aiUsefulInfoV4&Parts.go
+++ AI/aiUsefulInfoV4&Parts.go
@@ -7,18 +7,14 @@ import (
 )
 
 type AIUsefulInfoV4AndPartsCoefs struct {
-	CoefPlayByValueA float64
-	CoefPlayByValueB float64
-	CoefPlayByColorA float64
-	CoefPlayByColorB float64
-	CoefInfoValueA   float64
-	CoefInfoValueB   float64
-	CoefInfoColorA   float64
-	CoefInfoColorB   float64
-	CoefDiscardA     float64
-	CoefDiscardB     float64
-	CoefPlayA        float64
-	CoefPlayB        float64
+	CoefPlayByInfoA float64
+	CoefPlayByInfoB float64
+	CoefInfoA       float64
+	CoefInfoB       float64
+	CoefDiscardA    float64
+	CoefDiscardB    float64
+	CoefPlayA       float64
+	CoefPlayB       float64
 }
 
 type AIUsefulInfoV4AndParts struct {
@@ -31,47 +27,48 @@ func NewAIUsefulInfoV4AndParts(baseAI *BaseAI) *AIUsefulInfoV4AndParts {
 	ai := new(AIUsefulInfoV4AndParts)
 	ai.BaseAI = *baseAI
 	ai.Coefs = []AIUsefulInfoV4AndPartsCoefs{
+		/*
+			// Universal coefs
+			AIUsefulInfoV4AndPartsCoefs{
+				CoefPlayByInfoA: 2.47,
+				CoefPlayByInfoB: -0.07,
+				CoefInfoA:       2.7,
+				CoefInfoB:       -0.61,
+				CoefDiscardA:    0.7,
+				CoefDiscardB:    0.7,
+				CoefPlayA:       2.5,
+				CoefPlayB:       -0.6,
+			},
+		*/
 		AIUsefulInfoV4AndPartsCoefs{
-			CoefPlayByValueA: 2.6,
-			CoefPlayByValueB: 1.0,
-			CoefPlayByColorA: -1.0,
-			CoefPlayByColorB: 0.0,
-			CoefInfoValueA:   1.05,
-			CoefInfoValueB:   0.0,
-			CoefInfoColorA:   0.5,
-			CoefInfoColorB:   0.0,
-			CoefDiscardA:     0.0,
-			CoefDiscardB:     0.0,
-			CoefPlayA:        1.1,
-			CoefPlayB:        0.0,
+			CoefPlayByInfoA: 2.47,
+			CoefPlayByInfoB: -0.07,
+			CoefInfoA:       2.7,
+			CoefInfoB:       -0.61,
+			CoefDiscardA:    0.7,
+			CoefDiscardB:    0.7,
+			CoefPlayA:       2.5,
+			CoefPlayB:       -0.6,
 		},
 		AIUsefulInfoV4AndPartsCoefs{
-			CoefPlayByValueA: 1.1,
-			CoefPlayByValueB: 0.0,
-			CoefPlayByColorA: -1.0,
-			CoefPlayByColorB: 0.0,
-			CoefInfoValueA:   1.05,
-			CoefInfoValueB:   0.0,
-			CoefInfoColorA:   1.0,
-			CoefInfoColorB:   0.0,
-			CoefDiscardA:     1.0,
-			CoefDiscardB:     0.0,
-			CoefPlayA:        0.1,
-			CoefPlayB:        0.0,
+			CoefPlayByInfoA: 2.64,
+			CoefPlayByInfoB: 0.23,
+			CoefInfoA:       2.6,
+			CoefInfoB:       -0.06,
+			CoefDiscardA:    0.7,
+			CoefDiscardB:    0.7,
+			CoefPlayA:       2.6,
+			CoefPlayB:       -0.3,
 		},
 		AIUsefulInfoV4AndPartsCoefs{
-			CoefPlayByValueA: 1.1,
-			CoefPlayByValueB: 0.0,
-			CoefPlayByColorA: -1.0,
-			CoefPlayByColorB: 0.0,
-			CoefInfoValueA:   1.05,
-			CoefInfoValueB:   0.0,
-			CoefInfoColorA:   1.0,
-			CoefInfoColorB:   0.0,
-			CoefDiscardA:     1.0,
-			CoefDiscardB:     0.0,
-			CoefPlayA:        0.1,
-			CoefPlayB:        0.0,
+			CoefPlayByInfoA: 1.87,
+			CoefPlayByInfoB: 0.83,
+			CoefInfoA:       3.2,
+			CoefInfoB:       -0.61,
+			CoefDiscardA:    0.7,
+			CoefDiscardB:    0.7,
+			CoefPlayA:       4.5,
+			CoefPlayB:       0.7,
 		},
 	}
 	return ai
@@ -83,14 +80,10 @@ func (ai *AIUsefulInfoV4AndParts) GetCoefs(part int) []float64 {
 	}
 	coefs := ai.Coefs[part]
 	return []float64{
-		coefs.CoefPlayByValueA,
-		coefs.CoefPlayByValueB,
-		coefs.CoefPlayByColorA,
-		coefs.CoefPlayByColorB,
-		coefs.CoefInfoValueA,
-		coefs.CoefInfoValueB,
-		coefs.CoefInfoColorA,
-		coefs.CoefInfoColorB,
+		coefs.CoefPlayByInfoA,
+		coefs.CoefPlayByInfoB,
+		coefs.CoefInfoA,
+		coefs.CoefInfoB,
 		coefs.CoefDiscardA,
 		coefs.CoefDiscardB,
 		coefs.CoefPlayA,
@@ -103,18 +96,14 @@ func (ai *AIUsefulInfoV4AndParts) SetCoefs(part int, coefs ...float64) {
 		panic("Bad part for ai.SetCoefs()")
 	}
 	ai.Coefs[part] = AIUsefulInfoV4AndPartsCoefs{
-		CoefPlayByValueA: coefs[0],
-		CoefPlayByValueB: coefs[1],
-		CoefPlayByColorA: coefs[2],
-		CoefPlayByColorB: coefs[3],
-		CoefInfoValueA:   coefs[4],
-		CoefInfoValueB:   coefs[5],
-		CoefInfoColorA:   coefs[6],
-		CoefInfoColorB:   coefs[7],
-		CoefDiscardA:     coefs[8],
-		CoefDiscardB:     coefs[9],
-		CoefPlayA:        coefs[10],
-		CoefPlayB:        coefs[11],
+		CoefPlayByInfoA: coefs[0],
+		CoefPlayByInfoB: coefs[1],
+		CoefInfoA:       coefs[2],
+		CoefInfoB:       coefs[3],
+		CoefDiscardA:    coefs[4],
+		CoefDiscardB:    coefs[5],
+		CoefPlayA:       coefs[6],
+		CoefPlayB:       coefs[7],
 	}
 }
 
@@ -140,23 +129,16 @@ func (ai *AIUsefulInfoV4AndParts) GetAction() *game.Action {
 	ai.setAvailableInformation()
 	usefulActions := Actions{}
 	coefs := ai.Coefs[ai.GetPartOfGame()]
-	for color, tableCard := range info.TableCards {
-		for idx, card := range info.PlayerCards[myPos] {
-			if card.KnownColor && card.KnownValue && card.Color == color && card.Value == tableCard.Value+1 {
-				return game.NewAction(game.TypeActionPlaying, myPos, idx)
-			}
-		}
-	}
 
 	subHistory := ai.History[Max(len(ai.History)-len(info.PlayerCards)+1, 0):]
 	for i, action := range subHistory {
 		if action.ActionType == game.TypeActionInformationValue && action.PlayerPosition == myPos {
-			newCardIdxs := []int{}
+			newCardIdxs := map[int]int{}
 			step := len(ai.History) - len(subHistory) + i
 			oldInfo := ai.Informator.GetPlayerState(step)
 			for idx, card := range info.PlayerCards[myPos] {
 				if !oldInfo.PlayerCards[myPos][idx].KnownValue && card.KnownValue && card.Value == game.CardValue(action.Value) {
-					newCardIdxs = append(newCardIdxs, idx)
+					newCardIdxs[idx]++
 				}
 			}
 
@@ -185,11 +167,11 @@ func (ai *AIUsefulInfoV4AndParts) GetAction() *game.Action {
 				continue
 			}
 
-			for idx, card := range info.PlayerCards[myPos] {
-				if card.KnownValue && card.Value == game.CardValue(action.Value) {
+			for idx, _ := range info.PlayerCards[myPos] {
+				if _, ok := newCardIdxs[idx]; ok {
 					action := UsefulAction{
 						Action:     game.NewAction(game.TypeActionPlaying, myPos, idx),
-						Usefulness: coefs.CoefPlayByValueA/math.Sqrt(float64(len(newCardIdxs))) + coefs.CoefPlayByValueB,
+						Usefulness: coefs.CoefPlayByInfoA*float64(i)/float64(len(subHistory)) + coefs.CoefPlayByInfoB,
 					}
 					usefulActions = append(usefulActions, action)
 				}
@@ -197,12 +179,12 @@ func (ai *AIUsefulInfoV4AndParts) GetAction() *game.Action {
 		}
 
 		if action.ActionType == game.TypeActionInformationColor && action.PlayerPosition == myPos {
-			newCardIdxs := []int{}
+			newCardIdxs := map[int]int{}
 			step := len(ai.History) - len(subHistory) + i
 			oldInfo := ai.Informator.GetPlayerState(step)
 			for idx, card := range info.PlayerCards[myPos] {
 				if !oldInfo.PlayerCards[myPos][idx].KnownValue && card.KnownColor && card.Color == game.CardColor(action.Value) {
-					newCardIdxs = append(newCardIdxs, idx)
+					newCardIdxs[idx]++
 				}
 			}
 
@@ -231,11 +213,11 @@ func (ai *AIUsefulInfoV4AndParts) GetAction() *game.Action {
 				continue
 			}
 
-			for idx, card := range info.PlayerCards[myPos] {
-				if card.KnownColor && card.Color == game.CardColor(action.Value) {
+			for idx, _ := range info.PlayerCards[myPos] {
+				if _, ok := newCardIdxs[idx]; ok {
 					action := UsefulAction{
 						Action:     game.NewAction(game.TypeActionPlaying, myPos, idx),
-						Usefulness: coefs.CoefPlayByColorA/math.Sqrt(float64(len(newCardIdxs))) + coefs.CoefPlayByColorB,
+						Usefulness: coefs.CoefPlayByInfoA*float64(i)/float64(len(subHistory)) + coefs.CoefPlayByInfoB,
 					}
 					usefulActions = append(usefulActions, action)
 				}
@@ -246,27 +228,134 @@ func (ai *AIUsefulInfoV4AndParts) GetAction() *game.Action {
 	if info.BlueTokens > 0 {
 		for i := 1; i < len(info.PlayerCards); i++ {
 			nextPos := (myPos + i) % len(info.PlayerCards)
-			for color, tableCard := range info.TableCards {
-				for idx, card := range info.PlayerCards[nextPos] {
-					if card.Color == color && card.Value == tableCard.Value+1 {
-						cardInfo := &info.PlayerCardsInfo[nextPos][idx]
-						if !cardInfo.KnownValue {
-							action := UsefulAction{
-								Action:     game.NewAction(game.TypeActionInformationValue, nextPos, int(card.Value)),
-								Usefulness: coefs.CoefInfoValueA*(1.0-float64(i)/float64(len(info.PlayerCards))) + coefs.CoefInfoValueB - float64(i)/10,
-							}
-							usefulActions = append(usefulActions, action)
-						}
+			cards := info.PlayerCards[nextPos]
+			cardsInfo := info.PlayerCardsInfo[nextPos]
 
-						if !cardInfo.KnownColor {
-							action := UsefulAction{
-								Action:     game.NewAction(game.TypeActionInformationColor, nextPos, int(card.Color)),
-								Usefulness: coefs.CoefInfoColorA*(1.0-float64(i)/float64(len(info.PlayerCards))) + coefs.CoefInfoColorB - float64(i)/10,
+			usefulCards := []int{}
+			for idx, card := range cards {
+				tableCard := info.TableCards[card.Color]
+				if card.Value == tableCard.Value+1 {
+					usefulCards = append(usefulCards, idx)
+				}
+			}
+
+			if len(usefulCards) == 0 {
+				continue
+			}
+
+			maxUsefulByValue := 0
+			maxUsefulByColor := 0
+			maxNeutralByValue := 0
+			maxNeutralByColor := 0
+			minAdverseByValue := 5
+			minAdverseByColor := 5
+			var minCardValueByValue game.CardValue
+			var minCardValueByColor game.CardValue
+			var idxValue int
+			var idxColor int
+			subHistory := ai.History[Max(len(ai.History)-len(info.PlayerCards)+1-i, 0):]
+			infoColors := map[game.CardColor]struct{}{}
+			infoValues := map[game.CardValue]struct{}{}
+			for _, action := range subHistory {
+				if action.PlayerPosition != nextPos {
+					continue
+				}
+				if action.ActionType == game.TypeActionInformationValue {
+					infoValues[game.CardValue(action.Value)] = struct{}{}
+				}
+
+				if action.ActionType == game.TypeActionInformationColor {
+					infoColors[game.CardColor(action.Value)] = struct{}{}
+				}
+			}
+
+			for _, idx := range usefulCards {
+				card := &cards[idx]
+				cardInfo := &cardsInfo[idx]
+				usefulByValue := 0
+				usefulByColor := 0
+				neutralByValue := 0
+				neutralByColor := 0
+				adverseByValue := 0
+				adverseByColor := 0
+				for j := 0; j < len(cards); j++ {
+					if _, ok := infoValues[card.Value]; !ok && cards[j].Value == card.Value {
+						if info.TableCards[cards[j].Color].Value+1 == cards[j].Value {
+							if !cardInfo.KnownValue {
+								usefulByValue++
 							}
-							usefulActions = append(usefulActions, action)
+						} else if cardInfo.KnownColor {
+							neutralByValue++
+						} else {
+							adverseByValue++
 						}
 					}
+
+					if _, ok := infoColors[card.Color]; !ok && cards[j].Color == card.Color {
+						if info.TableCards[cards[j].Color].Value+1 == cards[j].Value {
+							if !cardInfo.KnownColor {
+								usefulByColor++
+							}
+						} else if cardInfo.KnownValue {
+							neutralByColor++
+						} else {
+							adverseByColor++
+						}
+					}
+
 				}
+
+				isBetter := adverseByValue < minAdverseByValue ||
+					adverseByValue == minAdverseByValue && card.Value < minCardValueByValue ||
+					adverseByValue == minAdverseByValue && card.Value == minCardValueByValue && usefulByValue > maxUsefulByValue ||
+					adverseByValue == minAdverseByValue && card.Value == minCardValueByValue && usefulByValue == maxUsefulByValue && neutralByValue > maxNeutralByValue
+				if isBetter {
+					maxUsefulByValue = usefulByValue
+					maxNeutralByValue = neutralByValue
+					minAdverseByValue = adverseByValue
+					minCardValueByValue = card.Value
+					idxValue = idx
+				}
+
+				isBetter = adverseByColor < minAdverseByColor ||
+					adverseByColor == minAdverseByColor && card.Value < minCardValueByColor ||
+					adverseByColor == minAdverseByColor && card.Value == minCardValueByColor && usefulByColor > maxUsefulByColor ||
+					adverseByColor == minAdverseByColor && card.Value == minCardValueByColor && usefulByColor == maxUsefulByColor && neutralByColor > maxNeutralByColor
+				if isBetter {
+					maxUsefulByColor = usefulByColor
+					maxNeutralByColor = neutralByColor
+					minAdverseByColor = adverseByColor
+					minCardValueByColor = card.Value
+					idxColor = idx
+				}
+			}
+
+			if maxUsefulByValue == 0 && maxUsefulByColor == 0 && maxNeutralByValue == 0 && maxNeutralByColor == 0 {
+				continue
+			}
+
+			isBetterInfoByValue := minAdverseByValue < minAdverseByColor ||
+				minAdverseByValue == minAdverseByColor && minCardValueByValue < minCardValueByColor ||
+				minAdverseByValue == minAdverseByColor && minCardValueByValue == minCardValueByColor && maxUsefulByValue > maxUsefulByColor ||
+				minAdverseByValue == minAdverseByColor && minCardValueByValue == minCardValueByColor && maxUsefulByValue == maxUsefulByColor && maxNeutralByValue > maxNeutralByColor ||
+				minAdverseByValue == minAdverseByColor && minCardValueByValue == minCardValueByColor && maxUsefulByValue == maxUsefulByColor && maxNeutralByValue == maxNeutralByColor
+
+			if isBetterInfoByValue {
+				card := &cards[idxValue]
+				bonus := -float64(i)/1000 - float64(minAdverseByValue)/1001 + float64(maxUsefulByValue)/100 + float64(maxNeutralByValue)/1002
+				action := UsefulAction{
+					Action:     game.NewAction(game.TypeActionInformationValue, nextPos, int(card.Value)),
+					Usefulness: coefs.CoefInfoA*(1.0-float64(i)/float64(len(info.PlayerCards))+bonus) + coefs.CoefInfoB,
+				}
+				usefulActions = append(usefulActions, action)
+			} else {
+				card := &cards[idxColor]
+				bonus := -float64(i)/1000 - float64(minAdverseByColor)/1001 + float64(maxUsefulByColor)/100 + float64(maxNeutralByColor)/1002
+				action := UsefulAction{
+					Action:     game.NewAction(game.TypeActionInformationColor, nextPos, int(card.Color)),
+					Usefulness: coefs.CoefInfoA*(1.0-float64(i)/float64(len(info.PlayerCards))+bonus) + coefs.CoefInfoB,
+				}
+				usefulActions = append(usefulActions, action)
 			}
 		}
 	}
@@ -275,8 +364,18 @@ func (ai *AIUsefulInfoV4AndParts) GetAction() *game.Action {
 		usefulnessNow := 0.0
 		usefulnessPotential := 0.0
 		if card.KnownColor && card.KnownValue {
-			usefulnessNow = 1.0
-			usefulnessPotential = 1.0
+			tableValue := info.TableCards[card.Color].Value
+			if tableValue+1 == card.Value {
+				usefulnessNow = 1.0
+			} else {
+				usefulnessNow = 0.0
+			}
+
+			if tableValue < card.Value {
+				usefulnessPotential = 1.0
+			} else {
+				usefulnessPotential = 0.0
+			}
 		} else {
 			for hashValue, probability := range card.ProbabilityCard {
 				color, value := game.ColorValueByHashColorValue(hashValue)
@@ -297,28 +396,26 @@ func (ai *AIUsefulInfoV4AndParts) GetAction() *game.Action {
 		if info.BlueTokens < game.MaxBlueTokens {
 			actionDiscard := UsefulAction{
 				Action:     game.NewAction(game.TypeActionDiscard, myPos, idx),
-				Usefulness: coefs.CoefDiscardA*math.Pow(1-usefulnessPotential, 1) + coefs.CoefDiscardB,
+				Usefulness: coefs.CoefDiscardA*math.Sin(math.Pow(1-usefulnessPotential, 2)*math.Pi/2) + coefs.CoefDiscardB - float64(idx)/1000,
 			}
 			usefulActions = append(usefulActions, actionDiscard)
 		}
 
 		actionPlay := UsefulAction{
 			Action:     game.NewAction(game.TypeActionPlaying, myPos, idx),
-			Usefulness: coefs.CoefPlayA*math.Pow(usefulnessNow, 1) + coefs.CoefPlayB,
+			Usefulness: coefs.CoefPlayA*math.Sin(math.Pow(usefulnessNow, 2)*math.Pi/2) + coefs.CoefPlayB - float64(idx)/1000,
 		}
 		usefulActions = append(usefulActions, actionPlay)
 	}
 
 	if len(usefulActions) > 0 {
-		var bestAction *game.Action
-		var topUsefulness float64
-		for i := 0; i < len(usefulActions); i++ {
-			if bestAction == nil || topUsefulness < usefulActions[i].Usefulness {
-				bestAction = usefulActions[i].Action
-				topUsefulness = usefulActions[i].Usefulness
+		bestActionIdx := 0
+		for i := 1; i < len(usefulActions); i++ {
+			if usefulActions.Less(i, bestActionIdx) {
+				bestActionIdx = i
 			}
 		}
-		return bestAction
+		return usefulActions[bestActionIdx].Action
 	}
 	return ai.getActionSmartyRandom()
 }
